8473f58da43873738fc203d88328d7d67126261f
Christina <chrisnby@stud.ntnu.no>
2025-02-20 09:21:49 +0100

    feat(Tile): add accessor method for nextTile and javadoc - implementing minimal features to test the player class

--- src/main/java/edu/ntnu/idi/idatt/Tile.java
+++ src/main/java/edu/ntnu/idi/idatt/Tile.java
@@ -1,13 +1,25 @@
 package edu.ntnu.idi.idatt;
 
+/**
+ * Represents a tile on the board. A tile has a unique ID and a reference to the next tile.
+ *
+ *@version 1.0
+ *
+ */
 public class Tile {
 
   private Tile nextTile;
   private int tileId;
   private TileAction landAction;
 
+  /**
+   * Constructs a new Tile with the specified tile ID.
+   *
+   * @param tileId the unique ID of the tile
+   */
   public Tile(int tileId) {
     this.tileId = tileId;
+    this.nextTile = null;
   }
 
   /**
@@ -19,6 +31,24 @@ public class Tile {
     return tileId;
   }
 
+  /**
+   * Accessor method for nextTile.
+   *
+   * @return the next tile
+   */
+  public Tile getNextTile() {
+    return nextTile;
+  }
+
+  /**
+   * Mutator method for nextTile.
+   *
+   * @param nextTile the next tile
+   */
+  public void setNextTile(Tile nextTile) {
+    this.nextTile = nextTile;
+  }
+
   public void landPlayer(Player player) {
 
   }
@@ -27,9 +57,6 @@ public class Tile {
 
   }
 
-  public void setNextTile(Tile nextTile) {
-    this.nextTile = nextTile;
-  }
 }
 /*Representerer ett felt på spillbrettet. I denne mappen har vi sagt at felt skal henge etter hverandre.
 Derfor har et felt alltid en referanse til «neste felt». Dersom det ikke finnes et «neste felt» er feltet det
