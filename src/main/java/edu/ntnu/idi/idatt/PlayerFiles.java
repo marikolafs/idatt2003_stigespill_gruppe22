9782f8072a1bd5a4c33464aaecd555e7cc054c82
Maria <145002050+marikolafs@users.noreply.github.com>
2025-03-05 12:24:10 +0100

    Refactor(PlayerFiles): Adding comments

--- src/main/java/edu/ntnu/idi/idatt/PlayerFiles.java
+++ src/main/java/edu/ntnu/idi/idatt/PlayerFiles.java
@@ -11,16 +11,26 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-
+/**
+ * The PlayerFiles class is responsible for reading and writing a list of players in csv files.
+ */
 public class PlayerFiles {
 
   private List<Player> players;
   static BoardGame game = new BoardGame();
 
+  /**
+   * Constructor initializing the player list.
+   */
   public PlayerFiles() {
     players = new ArrayList<>();
   }
 
+  /**
+   * Method that writes players to a csv file.
+   *
+   * @param players the list of players to be saved to a file.
+   */
   public static void writePlayersToFile(List<Player> players) {
     BufferedWriter writer = null;
     try {
@@ -38,6 +48,11 @@ public class PlayerFiles {
     }
     }
 
+  /**
+   * Method that parses a file and returns players.
+   *
+   * @param file the file the list of players is to be read from.
+   */
   public static void addPlayersThroughFile(File file){
     try (BufferedReader bufferedReader = new BufferedReader(new FileReader(file))){
       String line;
