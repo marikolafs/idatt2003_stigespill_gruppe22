1d611fefd5ae279da4179bbc2ce26b13b1d90f17
Maria <145002050+marikolafs@users.noreply.github.com>
2025-04-09 13:40:48 +0200

    feat(BoardFileWriter, BoardFileWriterGson): added methods to write the game board to a json file

--- src/main/java/edu/ntnu/idi/idatt/io/BoardFileWriter.java
+++ src/main/java/edu/ntnu/idi/idatt/io/BoardFileWriter.java
@@ -7,8 +7,14 @@ public interface BoardFileWriter {
 
   public void writeBoard(Board board) throws IOException;
 
-  public static void handleFileError(IOException e) {
-    System.out.println(e.getMessage());
+  /**
+   * Default method to handle errors during file writing.
+   * Can be used in implementing classes for logging purposes.
+   *
+   * @param e the IOExeption encountered during file processing
+   */
+  default void handleFileError(IOException e) {
+    System.err.println("Error writing board file: " + e.getMessage());
   }
 }
 
