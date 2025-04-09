3764d577c2983078c7b8253a615dafae76b2054b
Maria <145002050+marikolafs@users.noreply.github.com>
2025-04-09 14:06:54 +0200

    refactor(BoardGameFactory): modifying methods to use rows and columns

--- src/main/java/edu/ntnu/idi/idatt/BoardGameFactory.java
+++ src/main/java/edu/ntnu/idi/idatt/BoardGameFactory.java
@@ -2,22 +2,25 @@ package edu.ntnu.idi.idatt;
 
 import edu.ntnu.idi.idatt.engine.BoardGame;
 import edu.ntnu.idi.idatt.model.Board;
+import edu.ntnu.idi.idatt.model.Tile;
+import edu.ntnu.idi.idatt.model.actions.TileAction;
 
 public class BoardGameFactory {
+  private static TileAction ladderAction;
 
-  public static BoardGame createLadderGame(int tiles) throws Exception {
-    BoardGame game = createGame("Ladder game", tiles);
-    Board board = game.getBoard();
-
-    board.addLadder(1, 2);
+  public static BoardGame createLadderGame(int columns, int rows) throws Exception {
+    BoardGame game = createGame("Ladder game", 2, columns, rows);
+    game.setName("Ladder game");
+    game.setDescription("Ladder game");
+    Tile tile = game.getBoard().getTile(2);
     return game;
   }
 
-  private static BoardGame createGame(String gameName, int tiles) throws Exception {
+  private static BoardGame createGame(String gameName, int numberOfDice, int columns, int rows) throws Exception {
     BoardGame game = BoardGame.getInstance("default name", "default description");
-    game.createBoard(tiles);
-    game.createDice(2);
-    System.out.println("Created game: " + gameName + " with " + tiles + " tiles.");
+    game.createBoard(columns, rows);
+    game.createDice(numberOfDice);
+    System.out.println("Created game: " + gameName + " with " + rows * columns + " tiles.");
     return game;
   }
 }
