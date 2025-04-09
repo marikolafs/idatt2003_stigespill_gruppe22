08c414d17e944aeb19a01a04e4a1c50cfb2fe85a
Maria <145002050+marikolafs@users.noreply.github.com>
2025-04-09 14:20:49 +0200

    test(BoardFileWriterGsonTest): added test for columns and rows

--- src/test/java/edu/ntnu/idi/idatt/io/BoardFileWriterGsonTest.java
+++ src/test/java/edu/ntnu/idi/idatt/io/BoardFileWriterGsonTest.java
@@ -1,21 +1,19 @@
 package edu.ntnu.idi.idatt.io;
 
-import edu.ntnu.idi.idatt.model.actions.LadderAction;
 import edu.ntnu.idi.idatt.engine.BoardGame;
 import edu.ntnu.idi.idatt.model.Board;
 import edu.ntnu.idi.idatt.model.Tile;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
-import java.io.IOException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-
-import static org.junit.jupiter.api.Assertions.*;
-
 class BoardFileWriterGsonTest {
+
   private BoardFileWriterGson boardFileWriterGson;
   private BoardFileReaderGson boardFileReaderGson;
   private Path testBoardPath;
@@ -58,6 +56,17 @@ class BoardFileWriterGsonTest {
       board = boardFileReaderGson.readBoard(testBoardPath);
       game = BoardGame.getInstance("default name", "default description");
       assertEquals(2, board.getTile(1).getNextTile().getTileId());
+    }
+
+    @Test
+    @DisplayName("Should find rows and columns")
+    void findRowsAndColumns() throws IOException {
+      boardFileReaderGson = new BoardFileReaderGson();
+      testBoardPath = Paths.get("src/main/resources/board.json");
+      board = boardFileReaderGson.readBoard(testBoardPath);
+      game = BoardGame.getInstance("default name", "default description");
+      assertEquals(2, board.getRows());
+      assertEquals(1, board.getColumns());
     }
   }
 }
