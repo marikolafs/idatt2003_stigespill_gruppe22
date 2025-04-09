3c29788aa0239d4d5e8a8181fc24b684de3bbff2
Maria <145002050+marikolafs@users.noreply.github.com>
2025-04-09 16:20:33 +0200

    refactor(BoardGameFactory): edited the class to create two types of boards, each with set ladders

--- src/main/java/edu/ntnu/idi/idatt/BoardGameFactory.java
+++ src/main/java/edu/ntnu/idi/idatt/BoardGameFactory.java
@@ -1,27 +1,37 @@
 package edu.ntnu.idi.idatt;
 
 import edu.ntnu.idi.idatt.engine.BoardGame;
-import edu.ntnu.idi.idatt.model.Tile;
-import edu.ntnu.idi.idatt.model.actions.TileAction;
 
 public class BoardGameFactory {
 
-  private static TileAction ladderAction;
-
-  public static BoardGame createLadderGame(int columns, int rows) throws Exception {
-    BoardGame game = createGame("Ladder game", 2, columns, rows);
-    game.setName("Ladder game");
-    game.setDescription("Ladder game");
-    Tile tile = game.getBoard().getTile(2);
-    return game;
-  }
-
-  private static BoardGame createGame(String gameName, int numberOfDice, int columns, int rows)
+  private static BoardGame createGame(String gameName, String gameDescription, int numberOfDice,
+      int columns, int rows)
       throws Exception {
-    BoardGame game = BoardGame.getInstance("default name", "default description");
+    BoardGame game = BoardGame.getInstance(BoardGame.getName(), BoardGame.getDescription());
+    game.setName(gameName);
+    game.setDescription(gameDescription);
     game.createBoard(columns, rows);
     game.createDice(numberOfDice);
     System.out.println("Created game: " + gameName + " with " + rows * columns + " tiles.");
+    return game;
+  }
+
+  public static BoardGame createLadderGame90() throws Exception {
+    BoardGame game = createGame("Ladder game 90", "Ladder game with 90 tiles", 2, 9, 10);
+    game.getBoard().addLadder(4, 14);
+    game.getBoard().addLadder(9, 31);
+    game.getBoard().addLadder(20, 42);
+    game.getBoard().addLadder(27, 84);
+    game.getBoard().addLadder(51, 67);
+    game.getBoard().addLadder(73, 86);
+    return game;
+  }
+
+  public static BoardGame createLadderGame45() throws Exception {
+    BoardGame game = createGame("Ladder game 45", "Ladder game with 45 tiles", 2, 9, 5);
+    game.getBoard().addLadder(4, 14);
+    game.getBoard().addLadder(9, 31);
+    game.getBoard().addLadder(20, 42);
     return game;
   }
 }
