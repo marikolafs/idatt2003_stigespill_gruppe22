38ddb26dadcd0f03ba7377e8761bc79fdac295de
Christina Bye <ctinabye@outlook.com>
2025-04-10 13:30:10 +0200

    feat(Observable): add getObservers method

--- src/main/java/edu/ntnu/idi/idatt/observer/Observable.java
+++ src/main/java/edu/ntnu/idi/idatt/observer/Observable.java
@@ -11,14 +11,38 @@ public abstract class Observable {
         this.observers = new ArrayList<>();
     }
 
+    /**
+     * Gets the list of observers.
+     *
+     * @return the list of observers
+     */
+    public List<BoardGameObserver> getObservers() {
+        return observers;
+    }
+
+    /**
+     * Adds an observer to the list of observers.
+     *
+     * @param observer the observer to be added
+     */
     public void addObserver(BoardGameObserver observer) {
         observers.add(observer);
     }
 
+    /**
+     * Removes an observer from the list of observers.
+     *
+     * @param observer the observer to be removed
+     */
     public void removeObserver(BoardGameObserver observer) {
         observers.remove(observer);
     }
 
+    /**
+     * Notifies all observers of a state change.
+     *
+     * @param event the event to be passed to the observers
+     */
     public void notifyObservers(GameEvent event) {
         for (BoardGameObserver observer : observers) {
         observer.stateChanged(event);
