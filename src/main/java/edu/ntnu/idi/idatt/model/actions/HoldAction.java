db4abe79ecda7181fc9b18d83bcd3d3d0a63cab8
Maria <145002050+marikolafs@users.noreply.github.com>
2025-04-10 12:27:33 +0200

    feat(HoldAction): new class for a hold action

---
+++ src/main/java/edu/ntnu/idi/idatt/model/actions/HoldAction.java
@@ -0,0 +1,12 @@
+package edu.ntnu.idi.idatt.model.actions;
+
+import edu.ntnu.idi.idatt.model.Player;
+
+public class HoldAction implements TileAction {
+
+  @Override
+  public void perform(Player player){
+
+  }
+
+}
