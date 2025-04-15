67897f6eb350790546ba0bc7d43e65071b3b5770
Maria <145002050+marikolafs@users.noreply.github.com>
2025-04-15 14:11:08 +0200

    feat(LadderGameView): added visual for tiles

--- src/main/java/edu/ntnu/idi/idatt/view/LadderGameView.java
+++ src/main/java/edu/ntnu/idi/idatt/view/LadderGameView.java
@@ -1,24 +1,62 @@
 package edu.ntnu.idi.idatt.view;
 
+import edu.ntnu.idi.idatt.BoardGameFactory;
 import edu.ntnu.idi.idatt.engine.BoardGame;
+import edu.ntnu.idi.idatt.model.Board;
+import edu.ntnu.idi.idatt.model.Player;
+import edu.ntnu.idi.idatt.model.Tile;
+import java.util.Map;
 import javafx.application.Application;
+import javafx.geometry.HPos;
+import javafx.geometry.Pos;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
 public class LadderGameView {
 
-  private BoardGame boardGame;
+  BoardGameFactory boardGameFactory = new BoardGameFactory();
+  BoardGame game = BoardGame.getInstance(BoardGame.getName(),BoardGame.getDescription());
+  Board board =  game.getBoard();
+  Map<Integer, Tile> tiles = board.getTiles();
 
-  Group root = new Group();
-  Scene scene = new Scene(root, 300, 300, Color.OLIVE);
+  public LadderGameView(Stage stage) throws Exception {
+    BorderPane layout = new BorderPane();
+    Scene scene = new Scene(layout, 300, 300, Color.WHITE);
 
-  public LadderGameView() {
+    GridPane gridPane = new GridPane();
 
+    for (Tile  tile : tiles.values()) {
+      StackPane tilePane = new StackPane();
+      Label label = new Label("tile: " + tile.getTileId());
+      Rectangle r = new Rectangle();
+      r.setX(tile.getX());
+      r.setY(tile.getY());
+      r.setWidth(50);
+      r.setHeight(50);
+      if(tile.getTileId() % 2 == 0){
+        r.setFill(Color.BEIGE);
+      } else {
+        r.setFill(Color.OLIVE);
+      }
+      tilePane.getChildren().addAll(r, label);
+      gridPane.add(tilePane, tile.getX(), tile.getY());
+      System.out.println(tile.getTileId() + " " + tile.getX() + " " + tile.getY());
+    }
+
+    gridPane.setAlignment(Pos.CENTER);
+
+    stage.setTitle("Ladder Game");
+    scene.setRoot(gridPane);
+    stage.setScene(scene);
   }
-
 }
