ae3289b54955cf4ac82f66093011bedd3fa70159
Maria <145002050+marikolafs@users.noreply.github.com>
2025-04-15 14:10:29 +0200

    refactor(BoardGameApp): removing misplaced code

--- src/main/java/edu/ntnu/idi/idatt/ui/BoardGameApp.java
+++ src/main/java/edu/ntnu/idi/idatt/ui/BoardGameApp.java
@@ -1,9 +1,49 @@
 package edu.ntnu.idi.idatt.ui;
 
-public class BoardGameApp {
+import edu.ntnu.idi.idatt.BoardGameFactory;
+import edu.ntnu.idi.idatt.PlayerFiles;
+import edu.ntnu.idi.idatt.engine.BoardGame;
+import edu.ntnu.idi.idatt.model.Board;
+import edu.ntnu.idi.idatt.model.Player;
+import edu.ntnu.idi.idatt.model.Tile;
+import edu.ntnu.idi.idatt.view.LadderGameView;
+import java.io.File;
+import java.util.List;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
 
-  public void run() {
+public class BoardGameApp extends Application {
 
+  BoardGame game = BoardGame.getInstance(BoardGame.getName(), BoardGame.getDescription());
+  Board board = new Board();
+  BoardGameFactory boardGameFactory = new BoardGameFactory();
+  private static List<Player> players;
+
+  /*
+  Mål for påsken
+  - fullføre designmønstre
+  - sette opp basic gui
+  - ferdig gjøre hold action
+  - fikse example board filen
+  - evt annet?
+   */
+
+  @Override
+  public void start(Stage stage) throws Exception {
+    BoardGameFactory boardGameFactory = new BoardGameFactory();
+    boardGameFactory.createLadderGame90();
+    LadderGameView ladderGameView = new LadderGameView(stage);
+    stage.setFullScreen(true);
+    stage.show();
   }
 
+  public static void main(String[] args) {
+    launch(args);
+  }
 }
