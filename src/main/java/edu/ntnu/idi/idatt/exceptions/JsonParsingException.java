34a33723722662878bac48b9ed3866efbe4b0886
Maria <145002050+marikolafs@users.noreply.github.com>
2025-04-23 12:32:07 +0200

    feat(JsonParsingException): added an exception class for errors in parsing JSON-files

---
+++ src/main/java/edu/ntnu/idi/idatt/exceptions/JsonParsingException.java
@@ -0,0 +1,13 @@
+package edu.ntnu.idi.idatt.exceptions;
+
+public class JsonParsingException extends RuntimeException {
+
+  public JsonParsingException(String message) {
+    super(message);
+  }
+
+  public JsonParsingException(String message, Throwable cause) {
+    super(message, cause);
+  }
+
+}
