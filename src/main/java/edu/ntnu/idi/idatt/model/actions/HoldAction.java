2a00a16a28cc6d527e01e85471d0fd2b04218244
Maria <145002050+marikolafs@users.noreply.github.com>
2025-04-23 13:04:34 +0200

    feat(HoldAction): new action that skips the next turn of the player that lands on a tile with the action

--- src/main/java/edu/ntnu/idi/idatt/model/actions/HoldAction.java
+++ src/main/java/edu/ntnu/idi/idatt/model/actions/HoldAction.java
@@ -4,9 +4,19 @@ import edu.ntnu.idi.idatt.model.Player;
 
 public class HoldAction implements TileAction {
 
-  @Override
-  public void perform(Player player){
+  private final String descrption;
 
+  public HoldAction(String descrption) {
+    this.descrption = descrption;
   }
 
+  public String getDescrption() {
+    return descrption;
+  }
+
+  @Override
+  public void perform(Player player){
+    player.setHoldAction(true);
+    System.out.println(player.getName() + " holds ");
+  }
 }
