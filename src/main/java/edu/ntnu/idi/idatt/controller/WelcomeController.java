18088322b17865f9413876d08177c4b82852a409
Maria <145002050+marikolafs@users.noreply.github.com>
2025-04-23 12:36:30 +0200

    new(WelcomeController, WelcomeView): new classes for welcome screen and choosing between games

---
+++ src/main/java/edu/ntnu/idi/idatt/controller/WelcomeController.java
@@ -0,0 +1,32 @@
+package edu.ntnu.idi.idatt.controller;
+
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class WelcomeController {
+
+  /*
+  public void ladderButtonEvent(Button button, Stage stage, Scene scene) throws Exception {
+
+    button.setOnAction(event -> {
+
+      try {
+        switchScene(stage, scene);
+      } catch (Exception e) {
+        throw new RuntimeException(e);
+      }
+    });
+  }
+
+  public void otherButton() {
+
+
+  }
+
+  public void switchScene(Stage stage, Scene scene) throws Exception {
+    stage.setScene(scene);
+  }
+*/
+}
---
+++ src/main/java/edu/ntnu/idi/idatt/view/WelcomeView.java
@@ -0,0 +1,27 @@
+package edu.ntnu.idi.idatt.view;
+
+import edu.ntnu.idi.idatt.controller.LadderGameController;
+import edu.ntnu.idi.idatt.controller.WelcomeController;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+public class WelcomeView {
+
+  WelcomeController welcomeController = new WelcomeController();
+
+  /*
+  public void showWelcome(Stage stage) throws Exception {
+
+    Group root = new Group();
+    Scene welcomeScene = new Scene(root);
+    Scene ladderScene = new Scene(root);
+
+    Button ladderButton = new Button("LadderGame");
+    root.getChildren().add(ladderButton);
+
+  }
+   */
+
+}
