f5358b2deb58e4ff84a5122c2e69f7b984ae96a9
Christina Bye <ctinabye@outlook.com>
2025-04-24 12:18:07 +0200

    feat(BoardGameApp): add observer

--- src/main/java/edu/ntnu/idi/idatt/ui/BoardGameApp.java
+++ src/main/java/edu/ntnu/idi/idatt/ui/BoardGameApp.java
@@ -4,9 +4,11 @@ import edu.ntnu.idi.idatt.BoardGameFactory;
 import edu.ntnu.idi.idatt.PlayerFiles;
 import edu.ntnu.idi.idatt.controller.PlayerController;
 import edu.ntnu.idi.idatt.engine.BoardGame;
+import edu.ntnu.idi.idatt.io.BoardFileReaderGson;
 import edu.ntnu.idi.idatt.model.Board;
 import edu.ntnu.idi.idatt.model.Player;
 import edu.ntnu.idi.idatt.model.Tile;
+import edu.ntnu.idi.idatt.observer.Observable;
 import edu.ntnu.idi.idatt.view.LadderGameView;
 import edu.ntnu.idi.idatt.view.PlayerView;
 import edu.ntnu.idi.idatt.view.View;
@@ -22,11 +24,11 @@ import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class BoardGameApp extends Application {
-
   BoardGame game = BoardGame.getInstance(BoardGame.getName(), BoardGame.getDescription());
   Board board = new Board();
   BoardGameFactory boardGameFactory = new BoardGameFactory();
   private static List<Player> players;
+  private Observable observable = BoardGame.getInstance(BoardGame.getName(), BoardGame.getDescription());
 
   /*
   Mål for påsken
@@ -50,7 +52,7 @@ public class BoardGameApp extends Application {
     View view = new View(stage);
     PlayerView playerView = view.getPlayerView();
     PlayerController playerController = new PlayerController(game.getDice(), playerView);
-
+    observable.addObserver(playerView);
 //    LadderGameView ladderGameView = new LadderGameView(stage);
 
 //    stage.setFullScreen(true);
