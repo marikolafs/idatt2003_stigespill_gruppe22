1960e9cba2ffbb8da0b3f86ad8ddd4cfe39b8b19
Maria <145002050+marikolafs@users.noreply.github.com>
2025-05-08 12:58:43 +0200

    feat(BoardGameFactory): added more ladders to createLadderGame90 and made a new method createLadderGame90Plus with added tileActions

--- src/main/java/edu/ntnu/idi/idatt/BoardGameFactory.java
+++ src/main/java/edu/ntnu/idi/idatt/BoardGameFactory.java
@@ -18,12 +18,34 @@ public class BoardGameFactory {
 
   public static BoardGame createLadderGame90() throws Exception {
     BoardGame game = createGame("Ladder game 90", "Ladder game with 90 tiles", 2, 9, 10);
-    game.getBoard().addLadder(4, 14);
-    game.getBoard().addLadder(9, 31);
-    game.getBoard().addLadder(20, 42);
-    game.getBoard().addLadder(27, 84);
-    game.getBoard().addLadder(51, 67);
-    game.getBoard().addLadder(73, 86);
+    game.getBoard().addLadder(2, 38);
+    game.getBoard().addLadder(9, 11);
+    game.getBoard().addLadder(26, 6);
+    game.getBoard().addLadder(27, 47);
+    game.getBoard().addLadder(44, 4);
+    game.getBoard().addLadder(46, 74);
+    game.getBoard().addLadder(53, 71);
+    game.getBoard().addLadder(59, 40);
+    game.getBoard().addLadder(65, 86);
+    game.getBoard().addLadder(70, 31);
+    game.getBoard().addLadder(79, 14);
+    game.getBoard().addLadder(84, 63);
+    return game;
+  }
+
+  public static BoardGame createLadderGame90Plus() throws Exception {
+    BoardGame game = createLadderGame90();
+    game.setName("Ladder game 90+");
+    game.setDescription("Ladder game with 90 tiles and added obstacles.");
+    game.getBoard().addReturn(5);
+    game.getBoard().addReturn(18);
+    game.getBoard().addReturn(23);
+    game.getBoard().addReturn(49);
+    game.getBoard().addReturn(75);
+    game.getBoard().addHold(21);
+    game.getBoard().addHold(33);
+    game.getBoard().addHold(58);
+    game.getBoard().addHold(88);
     return game;
   }
 
