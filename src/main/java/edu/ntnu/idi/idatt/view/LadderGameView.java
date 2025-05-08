13bc8a21d91aaaa3bdce192d546bf40fbe285272
Maria <145002050+marikolafs@users.noreply.github.com>
2025-05-08 10:43:57 +0200

    refactor(LadderGameView): moved code out of this class

--- src/main/java/edu/ntnu/idi/idatt/view/LadderGameView.java
+++ src/main/java/edu/ntnu/idi/idatt/view/LadderGameView.java
@@ -59,118 +59,6 @@ public class LadderGameView extends BorderPane {
     setBottom(playerView);
   }
 
-  public LadderGameView(Stage stage) throws Exception {
-    welcomeView(stage);
-    selectPlayerSetupView(stage, "LadderGame");
-    gameBoardView();
-    //playerView();
-    scene.setRoot(layout);
-    stage.setTitle("Ladder Game");
-    stage.setScene(welcomeScene);
-  }
-
-  /**
-   * View for the board and tiles, as well as the players on the board.
-   *
-   * @throws FileNotFoundException if an image file cannot be found.
-   */
-  //TODO edit to be updated by the observer
-  public void gameBoardView() throws FileNotFoundException {
-    GridPane gridPane = new GridPane();
-
-    // adding tiles and labelling them
-    for (Tile tile : tiles.values()) {
-      StackPane tilePane = new StackPane();
-      Label label = new Label("tile: " + tile.getTileId());
-      Rectangle r = new Rectangle();
-      r.setX(tile.getX());
-      r.setY(tile.getY());
-      r.setWidth(50);
-      r.setHeight(50);
-      if (tile.getTileId() % 2 == 0) {
-        r.setFill(Color.BEIGE);
-      } else {
-        r.setFill(Color.OLIVE);
-      }
-
-      tilePane.getChildren().addAll(r, label);
-      gridPane.add(tilePane, tile.getX(), tile.getY());
-    }
-
-    // Adding players to the tile they´re on
-    // Should be updated to replace the image based on the chosen player piece
-    for (Player player : players) {
-      StackPane playerPane = new StackPane();
-
-      FileInputStream inputstream = new FileInputStream("src/main/resources/images/pieces/kitty.png");
-      Image image = new Image(inputstream);
-      ImageView imageView = new ImageView(image);
-      imageView.setFitHeight(40);
-      imageView.setFitWidth(40);
-
-      Label label = new Label(player.getPiece());
-      label.setStyle("-fx-font-size: 5;");
-
-      playerPane.getChildren().addAll(imageView, label);
-      gridPane.add(playerPane, player.getCurrentTile().getX(), player.getCurrentTile().getY());
-    }
-
-    gridPane.setAlignment(Pos.CENTER);
-    layout.setCenter(gridPane);
-  }
-
-  /**
-   * View for currentPlayers menu
-   *
-   * @throws FileNotFoundException if the image file cannot be found
-   */
-
-  /*
-  public void playerView() throws FileNotFoundException {
-
-    HBox hBox = new HBox();
-
-    FileInputStream inputstream = new FileInputStream("src/main/resources/images/pieces/kitty.png");
-    Image image = new Image(inputstream);
-    ImageView imageView = new ImageView(image);
-    imageView.setFitHeight(50);
-    imageView.setFitWidth(50);
-
-    Button rollButton = new Button("Roll");
-    rollButton.setOnAction(e -> {
-      BoardGame.play();
-    });
-    Text playerName = new Text("Player Name");
-    Text tileName = new Text("Current Tile");
-
-    hBox.getChildren().addAll(imageView,  rollButton,  playerName, tileName);
-
-    layout.setBottom(hBox);
-  }
-
-   */
-
-  Group root = new Group();
-  Group root2 = new Group();
-  Group root3 = new Group();
-  Scene playerScene2 = new Scene(root3);
-  Scene playerScene = new Scene(root2, 300, 300, Color.WHITE);
-  Scene welcomeScene = new Scene(root, 300, 300, Color.WHITE);
-
-  public void welcomeView(Stage stage) throws FileNotFoundException {
-    TilePane tilePane = new TilePane();
-
-    Label welcomeLabel = new Label("Welcome! What game would you like to play?");
-
-    tilePane.setAlignment(Pos.CENTER);
-    tilePane.setHgap(10);
-    tilePane.setVgap(10);
-    tilePane.getChildren().addAll(welcomeLabel, ladderButton, ludoButton);
-    root.getChildren().addAll(tilePane);
-    game.setName("Select a game");
-    game.setDescription("Select a game");
-  }
-
   public void selectPlayerSetupView(Stage stage, String gameType) throws FileNotFoundException {
     Label label = new Label("How many players?");
     ComboBox<Integer> playerCountBox = new ComboBox<>();
@@ -285,4 +173,5 @@ public class LadderGameView extends BorderPane {
     alert.setContentText(message);
     alert.showAndWait();
   }
+
 }
