a4729849dfeb7ed1dd147a3b5136493bca9ff8a4
Maria <145002050+marikolafs@users.noreply.github.com>
2025-05-08 10:44:35 +0200

    refactor(BoardGameApp, GameBoardController): connected game setup with play

--- src/main/java/edu/ntnu/idi/idatt/controller/GameBoardController.java
+++ src/main/java/edu/ntnu/idi/idatt/controller/GameBoardController.java
@@ -1,4 +1,34 @@
 package edu.ntnu.idi.idatt.controller;
 
+import edu.ntnu.idi.idatt.Game;
+import edu.ntnu.idi.idatt.SceneManager;
+import edu.ntnu.idi.idatt.engine.BoardGame;
+import edu.ntnu.idi.idatt.view.GameBoardView;
+import edu.ntnu.idi.idatt.view.LadderGameView;
+import edu.ntnu.idi.idatt.view.PlayerView;
+import java.io.FileNotFoundException;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 public class GameBoardController {
+
+  BoardGame game = BoardGame.getInstance(BoardGame.getName(), BoardGame.getDescription());
+
+  public GameBoardController(Stage stage, SceneManager sceneManager) throws FileNotFoundException {
+    try{
+      GameBoardView gameBoardView = new GameBoardView(game.getBoard(), game.getPlayers());
+      PlayerView playerView = new PlayerView();
+
+      LadderGameView layout = new LadderGameView(gameBoardView, playerView);
+      Scene gameScene = new Scene(layout, 1000, 800);
+      sceneManager.switchScene(gameScene, "LadderGame");
+
+      PlayerController playerController = new PlayerController(game.getDice(), playerView);
+
+      game.addObserver(gameBoardView);
+      game.addObserver(playerView);
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
 }
--- src/main/java/edu/ntnu/idi/idatt/ui/BoardGameApp.java
+++ src/main/java/edu/ntnu/idi/idatt/ui/BoardGameApp.java
@@ -1,6 +1,8 @@
 package edu.ntnu.idi.idatt.ui;
 
 import edu.ntnu.idi.idatt.BoardGameFactory;
+import edu.ntnu.idi.idatt.SceneManager;
+import edu.ntnu.idi.idatt.controller.WelcomeController;
 import edu.ntnu.idi.idatt.io.PlayerFiles;
 import edu.ntnu.idi.idatt.controller.PlayerController;
 import edu.ntnu.idi.idatt.engine.BoardGame;
@@ -9,22 +11,36 @@ import edu.ntnu.idi.idatt.model.Player;
 import edu.ntnu.idi.idatt.observer.Observable;
 import edu.ntnu.idi.idatt.view.GameBoardView;
 import edu.ntnu.idi.idatt.view.LadderGameView;
+import edu.ntnu.idi.idatt.view.PlayerSetupView;
 import edu.ntnu.idi.idatt.view.PlayerView;
 import edu.ntnu.idi.idatt.view.View;
+import edu.ntnu.idi.idatt.view.WelcomeView;
 import java.util.List;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class BoardGameApp extends Application {
+
+  private Stage primaryStage;
+  private Scene scene;
+
   BoardGame game = BoardGame.getInstance(BoardGame.getName(), BoardGame.getDescription());
   Board board = new Board();
   BoardGameFactory boardGameFactory = new BoardGameFactory();
   private static List<Player> players;
-  private Observable observable = BoardGame.getInstance(BoardGame.getName(), BoardGame.getDescription());
+  private Observable observable = game;
 
   @Override
   public void start(Stage stage) throws Exception {
+    this.primaryStage = stage;
+    SceneManager sceneManager = new SceneManager(primaryStage);
+
+    new WelcomeController(primaryStage, sceneManager);
+
+    primaryStage.show();
+
+    /*
     boardGameFactory.createLadderGame90();
 
     game.addPlayer(new Player("player1", game, "piece1"));
@@ -34,20 +50,25 @@ public class BoardGameApp extends Application {
 
     game.play();
 
-    View view = new View(stage);
-    GameBoardView gameBoardView = view.getGameBoardView();
-    PlayerView playerView = view.getPlayerView();
+     */
 
-    LadderGameView ladderGameView = new  LadderGameView(gameBoardView, playerView);
-    Scene scene = new Scene(ladderGameView, 1000, 800);
-    stage.setScene(scene);
-    stage.setFullScreen(true);
-    stage.show();
+    //View view = new View(stage);
+    //GameBoardView gameBoardView = view.getGameBoardView();
+    //PlayerView playerView = view.getPlayerView();
+    //WelcomeView welcomeView = new WelcomeView();
 
-    PlayerController playerController = new PlayerController(game.getDice(), playerView);
-    observable.addObserver(playerView);
-    observable.addObserver(gameBoardView);
+    //LadderGameView ladderGameView = new  LadderGameView(gameBoardView, playerView);
+    //Scene scene = new Scene(welcomeView, 1000, 800);
+    //stage.setScene(scene);
+    //stage.setFullScreen(true);
+    //stage.show();
+
+    //PlayerController playerController = new PlayerController(game.getDice(), playerView);
+    //observable.addObserver(playerView);
+    //observable.addObserver(gameBoardView);
   }
+
+
 
   public static void main(String[] args) {
     launch(args);
