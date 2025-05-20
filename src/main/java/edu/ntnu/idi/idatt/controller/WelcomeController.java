99f5e990822afc4fbced99d1f7d8456fac860ba5
Christina Bye <ctinabye@outlook.com>
2025-05-20 15:02:27 +0200

    feat(*): add styling for buttons

--- src/main/java/edu/ntnu/idi/idatt/controller/WelcomeController.java
+++ src/main/java/edu/ntnu/idi/idatt/controller/WelcomeController.java
@@ -2,6 +2,7 @@ package edu.ntnu.idi.idatt.controller;
 
 import edu.ntnu.idi.idatt.SceneManager;
 import edu.ntnu.idi.idatt.view.WelcomeView;
+import java.io.FileNotFoundException;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
@@ -11,7 +12,7 @@ public class WelcomeController {
 
   private SceneManager sceneManager;
 
-  public WelcomeController(Stage stage, SceneManager sceneManager) {
+  public WelcomeController(Stage stage, SceneManager sceneManager) throws FileNotFoundException {
     WelcomeView welcomeView = new WelcomeView();
     Scene welcomeScene = new Scene(welcomeView.getView(), 400, 300);
 
--- src/main/java/edu/ntnu/idi/idatt/view/WelcomeView.java
+++ src/main/java/edu/ntnu/idi/idatt/view/WelcomeView.java
@@ -1,14 +1,19 @@
 package edu.ntnu.idi.idatt.view;
 
-import edu.ntnu.idi.idatt.controller.LadderGameController;
-import edu.ntnu.idi.idatt.controller.WelcomeController;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 import javafx.geometry.Pos;
-import javafx.scene.Group;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.VBox;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class WelcomeView {
@@ -16,10 +21,15 @@ public class WelcomeView {
   private final VBox root = new VBox(10);
   private final Button ladderButton = new Button("Ladder Game");
   private final Button ludoButton = new Button("Ludo");
+  private final Label welcomeLabel = new Label("Welcome!");
 
-  public WelcomeView() {
+  public WelcomeView() throws FileNotFoundException {
     root.setAlignment(Pos.CENTER);
-    root.getChildren().addAll(new Label("Welcome!"),ladderButton, ludoButton);
+    root.getChildren().addAll(welcomeLabel,ladderButton, ludoButton);
+
+    welcomeLabel.setStyle("-fx-font-size: 65px;");
+
+    setButtonStyle();
   }
 
   public VBox getView() {
@@ -32,5 +42,31 @@ public class WelcomeView {
 
   public Button getLudoButton() {
     return ludoButton;
+  }
+
+  public void setButtonStyle() throws FileNotFoundException {
+
+    FileInputStream ladderGameImageStream = new FileInputStream("src/main/resources/game_images/ladderGame.png");
+    FileInputStream ludoImageStream = new FileInputStream("src/main/resources/game_images/ludo.png");
+
+
+    Image ladderGame90Image = new Image(ladderGameImageStream);
+    Image ludoImage = new Image(ludoImageStream);
+
+
+    javafx.scene.image.ImageView ladderGameImageView = new javafx.scene.image.ImageView(ladderGame90Image);
+    javafx.scene.image.ImageView ludoImageView = new ImageView(ludoImage);
+
+
+    ladderGameImageView.setFitWidth(65);
+    ladderGameImageView.setFitHeight(65);
+    ludoImageView.setFitWidth(65);
+    ludoImageView.setFitHeight(65);
+
+
+    ladderButton.setStyle("-fx-background-color: OLIVE; -fx-font-size: 32px;");
+    ludoButton.setStyle("-fx-background-color: OLIVE; -fx-font-size: 32px;");
+    ladderButton.setGraphic(ladderGameImageView);
+    ludoButton.setGraphic(ludoImageView);
   }
 }
