e74fbfc9aae7c1e036c8edb2d24b166b9fda479e
Christina Bye <ctinabye@outlook.com>
2025-05-22 22:48:10 +0200

    feat(WelcomeController): add showWelcomeScene() and setupEventHandlers() to constructor

--- src/main/java/edu/ntnu/idi/idatt/controller/WelcomeController.java
+++ src/main/java/edu/ntnu/idi/idatt/controller/WelcomeController.java
@@ -4,18 +4,25 @@ import edu.ntnu.idi.idatt.SceneManager;
 import edu.ntnu.idi.idatt.view.WelcomeView;
 import java.io.FileNotFoundException;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class WelcomeController {
 
-  private SceneManager sceneManager;
+  private final SceneManager sceneManager;
+  private final WelcomeView welcomeView;
+  private final Scene welcomeScene;
 
   public WelcomeController(Stage stage, SceneManager sceneManager) throws FileNotFoundException {
-    WelcomeView welcomeView = new WelcomeView();
-    Scene welcomeScene = new Scene(welcomeView.getView(), 400, 300);
+    this.sceneManager = sceneManager;
+    this.welcomeView = new WelcomeView();
+    this.welcomeScene = new Scene(welcomeView.getView(), 400, 300);
 
+    showWelcomeScene();
+    setupEventHandlers(stage);
+    sceneManager.addScene("Welcome", welcomeScene);
+  }
+
+  private void setupEventHandlers(Stage stage) {
     welcomeView.getLadderButton().setOnAction(event -> {
       new PlayerSetupController(stage, sceneManager, "LadderGame");
     });
@@ -23,8 +30,9 @@ public class WelcomeController {
     welcomeView.getLudoButton().setOnAction(event -> {
       new PlayerSetupController(stage, sceneManager, "Ludo");
     });
-
-    sceneManager.switchScene(welcomeScene, "Welcome");
   }
 
+  public void showWelcomeScene() {
+    sceneManager.switchScene(welcomeScene, "Welcome");
+  }
 }
